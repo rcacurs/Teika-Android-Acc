f31ffb5a9da07e6d7a8612f7b82e59537c96494f
Ričards Cacurs <rcacurs@gmail.com>
2016-05-26 14:24:39 +0300

    delete unused modules

--- head-and-posture/src/main/java/lv/edi/HeadAndPosture/ProcessingEventListener.java
+++
@@ -1,9 +0,0 @@
-package lv.edi.HeadAndPosture;
-
-/**
- * Created by Richards on 19/06/2015.
- */
-public interface ProcessingEventListener {
-
-    public void onProcessingResult(ProcessingResult result);
-}
